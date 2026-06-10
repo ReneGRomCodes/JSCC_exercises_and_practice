d076c60064404f37198a5eab69f08290cefff5a0
rene romero <renegromcodes@gmail.com>
2026-06-10 11:34:00 +0200

    Added Ch2_Ex4.js.

---
+++ Ch2_Ex4.js
@@ -0,0 +1,7 @@
+/*
+Exercise 2-4: Create a string using all the escape sequences from 'Table 2-1'.
+ */
+
+const escapeSequences = "He said, \"Don\'t look back!\"\n\tPath: C:\\Users";
+
+console.log(escapeSequences);
