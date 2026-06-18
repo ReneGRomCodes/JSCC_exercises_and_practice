a9c7db84b877785e646a2f40ab5b74613c8de658
rene romero <renegromcodes@gmail.com>
2026-06-18 11:27:26 +0200

    Added Ch3_Ex3.js.

---
+++ Ch3_Ex3.js
@@ -0,0 +1,7 @@
+/*
+Exercise 3-3: Use the 'join' method to convert the array ["X", "X", "X"] into the string "XoXoX".
+ */
+const xArr = ["X", "X", "X"];
+const joinedArr = xArr.join("o");
+
+console.log(joinedArr);
